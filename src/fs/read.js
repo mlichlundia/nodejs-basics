77e803f703665776d2f8e57e180ffe648e6aed54
mlichlundia <mlichlundiapl@gmail.com>
2023-06-15 13:07:53 +0200

    feat: add read functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'node:fs/promises';
+import { getDirname } from '../utils/getDirname.js';
+import { ERROR_MESSAGE } from '../constants.js';
+import { isTargetExists } from '../utils/isTargetExists.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const read = async () => {
-    // Write your code here 
+    const filePath = __dirname + '/files/fileToRead.txt'
+    
+    if(await isTargetExists(filePath)) {
+        const fileContent = await readFile(filePath, {encoding: 'utf-8'})
+        console.log(fileContent)
+    } else {
+        throw Error(ERROR_MESSAGE)
+    }
 };
 
 await read();
