e61156f5f9e27ab3534d21bd966ceb519ff8fc92
mlichlundia <mlichlundiapl@gmail.com>
2023-06-15 12:29:21 +0200

    feat: add copy functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { cp } from 'node:fs/promises';
+import { getDirname } from '../utils/getDirname.js';
+import { ERROR_MESSAGE } from '../constants.js';
+import { isTargetExists } from '../utils/isTargetExists.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const copy = async () => {
-    // Write your code here 
+    const directoryPath = __dirname + '/files'
+    const copyPath = __dirname + '/files_copy'
+
+    if (await isTargetExists(directoryPath)) {
+        try {
+            await cp(directoryPath, copyPath, {errorOnExist: true, force: false, recursive: true })
+        } catch {
+            throw Error(ERROR_MESSAGE)
+        }
+    } else {
+        throw Error(ERROR_MESSAGE)
+    }
 };
 
 await copy();
