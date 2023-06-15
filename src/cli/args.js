bdf6b60a52a3d70dba3e4f45e5957d4729058379
mlichlundia <mlichlundiapl@gmail.com>
2023-06-15 14:33:12 +0200

    feat: add parseArgs functionality

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const prefix = '--'
+    const result = process.argv
+        .reduce((res, arg, i, arr) => {
+            if(!arg.startsWith(prefix)) {
+                return res
+            }
+
+            res = [...res, `${arg.replace(prefix, '')} is ${arr[i + 1]}`]
+            return res
+        }, [])
+        .join(', ')
+
+    console.log(result)
 };
 
 parseArgs();
