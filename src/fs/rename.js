518269be08165abca423a662fd319789d297840f
mlichlundia <mlichlundiapl@gmail.com>
2023-06-15 12:44:55 +0200

    feat: add rename functionality

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { rename as fsRename } from 'node:fs/promises';
+import { isTargetExists } from '../utils/isTargetExists.js';
+import { ERROR_MESSAGE } from '../constants.js';
+import { getDirname } from '../utils/getDirname.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const rename = async () => {
-    // Write your code here 
+    const targetPath = __dirname + '/files/wrongFilename.txt'
+    const replacePath = __dirname + '/files/properFilename.md'
+
+    console.log(await isTargetExists(targetPath), await isTargetExists(replacePath))
+
+    if(!(await isTargetExists(targetPath)) || (await isTargetExists(replacePath))) {
+        throw Error(ERROR_MESSAGE)
+    } else {
+        await fsRename(targetPath, replacePath)
+    }
 };
 
 await rename();
