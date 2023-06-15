05e6c8d7bd9184f80bef84ade77e38f1a465103a
mlichlundia <mlichlundiapl@gmail.com>
2023-06-15 11:46:51 +0200

    feat: add create functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import { access, appendFile, constants } from 'node:fs/promises';
+import { getDirname } from '../utils/getDirname.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const create = async () => {
-    // Write your code here 
+    const filePath = __dirname + '/files/fresh.txt'
+    const fileContent = 'I am fresh and young'
+    const errorMessage = 'FS operation failed'
+    
+    let isFileExists
+
+    try {
+        await access(filePath, constants.F_OK)
+        isFileExists = true
+    } catch {
+        isFileExists = false
+    }
+    
+    if(isFileExists) {
+        throw Error(errorMessage)
+    } else {
+        await appendFile(filePath, fileContent)
+    }
 };
 
 await create();
