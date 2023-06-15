b6080d74cd7563576aed1f64d8ca3ad72a5ee8e8
mlichlundia <mlichlundiapl@gmail.com>
2023-06-15 12:57:23 +0200

    feat: add delete functionality

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { rm } from 'node:fs/promises';
+import { getDirname } from '../utils/getDirname.js';
+import { ERROR_MESSAGE } from '../constants.js';
+import { isTargetExists } from '../utils/isTargetExists.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = __dirname + '/files/fileToRemove.txt'
+
+    if(await isTargetExists(filePath)) {
+        await rm(filePath)
+    } else {
+        throw Error(ERROR_MESSAGE)
+    }
 };
 
 await remove();
