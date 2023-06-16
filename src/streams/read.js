8e33cc12fb0e1a45df1cf83c992610244423cb3f
mlichlundia <mlichlundiapl@gmail.com>
2023-06-16 15:33:21 +0200

    feat: add stream read functionality

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs';
+import process from 'node:process';
+import { getDirname } from '../utils/getDirname.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const read = async () => {
-    // Write your code here 
+    const readFilePath = __dirname + '/files/fileToRead.txt'
+    const stream = createReadStream(readFilePath)
+    
+    stream.pipe(process.stdout)
 };
 
 await read();
