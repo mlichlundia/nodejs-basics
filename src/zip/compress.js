4bd2d5836518988267ffa020b4106bb6cb628507
mlichlundia <mlichlundiapl@gmail.com>
2023-06-16 16:33:12 +0200

    feat: add compress functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { createReadStream, createWriteStream } from 'node:fs'
+import { pipeline } from 'node:stream/promises';
+import { createGzip } from 'node:zlib'
+import { getDirname } from '../utils/getDirname.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = __dirname + '/files/fileToCompress.txt' 
+    const archPath = __dirname + '/files/archive.gz'
+
+    await pipeline(createReadStream(filePath), createGzip(), createWriteStream(archPath))
 };
 
 await compress();
