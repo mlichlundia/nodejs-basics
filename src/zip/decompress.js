6440ac0e2b427cf7232156d40b68a089ba3a9b7a
mlichlundia <mlichlundiapl@gmail.com>
2023-06-16 16:35:40 +0200

    feat: add decompress functionality

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { createReadStream, createWriteStream } from 'node:fs'
+import { pipeline } from 'node:stream/promises';
+import { createGunzip } from 'node:zlib'
+import { getDirname } from '../utils/getDirname.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const decompress = async () => {
-    // Write your code here 
+    const filePath = __dirname + '/files/fileToCompress.txt' 
+    const archPath = __dirname + '/files/archive.gz'
+
+    await pipeline(createReadStream(archPath), createGunzip(), createWriteStream(filePath))
 };
 
 await decompress();
