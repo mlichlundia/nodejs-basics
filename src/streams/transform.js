61dcd0d854731f018c0707f90052a3d9161fe082
mlichlundia <mlichlundiapl@gmail.com>
2023-06-16 16:09:53 +0200

    feat: add stream transform functionality

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import {Transform} from 'stream' 
+import {pipeline} from 'stream/promises'
+
+const reverse = (str) => str.toString().split('').reverse().join('')
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, reverse(chunk))
+        }
+    })
+
+    pipeline(process.stdin, transformStream, process.stdout)
 };
 
 await transform();
