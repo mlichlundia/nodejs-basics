9efe19339cda7e8aa354d47a74503ff7fa720bf0
mlichlundia <mlichlundiapl@gmail.com>
2023-06-16 15:57:29 +0200

    feat: add stream write functionality

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'node:fs';
+import { getDirname } from '../utils/getDirname.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const write = async () => {
-    // Write your code here 
+    const filePath = __dirname + '/files/fileToWrite.txt'
+    const stream = createWriteStream(filePath)
+
+    process.stdin.pipe(stream)
 };
 
 await write();
