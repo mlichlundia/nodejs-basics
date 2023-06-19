f549dda79a3f5dd99ad5bc41a2977da784f39711
mlichlundia <mlichlundiapl@gmail.com>
2023-06-19 14:39:26 +0200

    feat: add child process functionality

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
+import { fork } from 'child_process'
+import { getDirname } from '../utils/getDirname.js';
+
+const {__dirname} = getDirname(import.meta.url)
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const filePath = __dirname + '/files/script.js'
+    const childProcess = fork(filePath, args, {silent: true})
+
+    process.stdin.pipe(childProcess.stdin)
+    childProcess.stdout.pipe(process.stdout)
 };
 
 // Put your arguments in function call to test this functionality
