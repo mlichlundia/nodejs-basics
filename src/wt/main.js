71e1d3f48e318ca25bc164cc787e2b534d92390b
mlichlundia <mlichlundiapl@gmail.com>
2023-06-19 13:50:04 +0200

    feat: add worker threads functionality

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import os from 'os'
+import { getDirname } from '../utils/getDirname.js';
+import { Worker } from 'worker_threads';
+
+const {__dirname} = getDirname(import.meta.url)
+
+const promisify = (worker) => {
+    return new Promise((res) => {
+        worker.on('message', (value) => (res({status: 'resolved', data: value})))
+        worker.on('error', () => (res({status: 'error', data: null})))
+    })
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const filePath = __dirname + '/worker.js'
+    const defaultNumber = 10
+    const cpuCoresCount = os.cpus().length;
+    const promises = []
+    
+    for (let i = 0; i <= cpuCoresCount; i++) {
+        promises.push(promisify(new Worker(filePath, {workerData: defaultNumber + i})))
+    }
+
+    Promise.all(promises).then(console.log)
 };
 
 await performCalculations();
